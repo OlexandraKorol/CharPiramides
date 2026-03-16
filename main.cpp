494d5c1d11b78f7dc6f478e2b39f8674998047f0
Olexandra Korol <109950379+OlexandraKorol@users.noreply.github.com>
2026-03-17 00:09:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,3 @@
-//#include <stdio.h>
-//#include <iostream>
-//#include <string>
-//
-//using namespace std;
-//
-//// ітерацій - 5
-//// кожкого разу - додається символ
-//
-////  abc
-////  
-////  a
-//// abc
-////abcab
-////  
-//
-//
-//int main() {
-//    string userString {};
-//    cout << "Type your string there: " << endl;
-//    cin >> userString;
-//    string printedOutput {};
-//
-//    for (int i {0}; i < userString.length(); i++) {
-//        string blanks (userString.length() - i - 1, ' ');
-//        printedOutput += userString[i];
-//        
-//        cout << blanks << printedOutput << endl;
-//    };
-//    
-//};
-
-
 #include <iostream>
 #include <string>
 
